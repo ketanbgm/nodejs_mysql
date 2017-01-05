1c85a8b223badd4ea4e8ba308aa05ccc328e444c
ketanbgm <ketanbgm@gmail.com>
2017-01-05 18:42:09 +0530

    nodemon with select query

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -1,9 +1,23 @@
-module.exports = function() {
+module.exports = function(connection) {
 
 var router = require('express').Router();
 
 router.get('/test', function(req, res){
 	res.send('ok done');
+})
+
+router.post('/login', function(req, res){
+	var user_name = req.body.username;
+	var password =  req.body.password;
+	console.log(password)
+	   connection.query('SELECT * FROM users where username = ? and password = ?', [user_name, password], function(err, rows) {
+	   	 if (err) {
+               throw err;
+     		}
+     		else {
+     			res.send(rows)
+     		}
+	   })
 })
 	return router;
 }
--- server/index.js
+++ server/index.js
@@ -2,13 +2,27 @@ var express = require('express');
 var app = express();
 var bodyparser = require('body-parser');
 var mysql = require('mysql')
-var connection = require('./connection');
-
+/*var connection = require('./connection');
+*/
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
-app.use('/api/user', require('./api/user')());
 
-var server = app.listen(8000, function() {
+var connection = mysql.createConnection({
+	'host': 'localhost',
+	'user': 'root',
+	'password':'',
+	'database': 'nodemysql'
+})
+
+connection.connect(function (err) {
+	// body...
+	if (err) throw err
+		 console.log('You are now connected...');
+})
+
+var server = app.listen(7000, function() {
   console.log('Server listening on port ' + server.address().port);
+app.use('/api/user', require('./api/user')(connection));
+
 });
 
