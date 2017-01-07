e8afb92d49fab9fdd1686c16355b6d1d02bb21d4
ketanbgm <ketanbgm@gmail.com>
2017-01-07 13:52:35 +0530

    middleware implementation

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -19,5 +19,19 @@ router.post('/login', function(req, res){
      		}
 	   })
 })
+
+router.post('/login-test', function(req, res){
+	var user_name = req.body.username;
+	var password =  req.body.password;
+	console.log(password)
+	   connection.query('SELECT * FROM users where username = ? and password = ?', [user_name, password], function(err, rows) {
+	   	 if (err) {
+               throw err;
+     		}
+     		else {
+     			res.send(rows)
+     		}
+	   })
+})
 	return router;
 }
--- server/index.js
+++ server/index.js
@@ -2,27 +2,42 @@ var express = require('express');
 var app = express();
 var bodyparser = require('body-parser');
 var mysql = require('mysql')
-/*var connection = require('./connection');
-*/
-app.use(bodyparser.urlencoded({extended: true}));
+    /*var connection = require('./connection');
+     */
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
+
 var connection = mysql.createConnection({
-	'host': 'localhost',
-	'user': 'root',
-	'password':'',
-	'database': 'nodemysql'
+    'host': 'localhost',
+    'user': 'root',
+    'password': '',
+    'database': 'nodemysql'
 })
 
-connection.connect(function (err) {
-	// body...
-	if (err) throw err
-		 console.log('You are now connected...');
+connection.connect(function(err) {
+    // body...
+    if (err) throw err
+    console.log('You are now connected...');
 })
 
-var server = app.listen(7000, function() {
-  console.log('Server listening on port ' + server.address().port);
-app.use('/api/user', require('./api/user')(connection));
-
+app.use('/api/user/index', function(req, res, next) {
+    // invoked for any request starting with /users
+    var username = req.body.username;
+    console.log(username);
+    if (username) {
+        next();
+    }
+    else{
+    	res.send("Invalid username")
+    }
 });
 
+
+
+
+var server = app.listen(7000, function() {
+    console.log('Server listening on port ' + server.address().port);
+    app.use('/api/user/index', require('./api/user/index')(connection));
+    app.use('/api/user/login_reg', require('./api/user/login_reg')(connection));
+});
